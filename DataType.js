e72d77965986a843556968656be5782c00a3e41d
LuckyShilpa <87297336+LuckyShilpa@users.noreply.github.com>
2025-10-15 20:25:29 +0530

    Add files via upload

---
+++ DataType.js
@@ -0,0 +1,18 @@
+//DataType 
+
+console.log("Welcome to Playwright")
+let firstName = "Shilpa"
+console.log("Name : " + firstName ,"DataType   :"  +typeof(firstName))
+
+let companyName = "YZ"
+console.log("Company Name :" + companyName , "DataType :" +typeof(companyName))
+
+
+let mobileNumber = 9880847335n
+console.log("Mobile No :" + mobileNumber , "DataType :" +typeof(mobileNumber))
+
+let  isAutomation = true
+console.log("Automation  :" + isAutomation , "DataType :" +typeof(isAutomation))
+
+let hasPlaywright
+console.log(typeof(hasPlaywright))
